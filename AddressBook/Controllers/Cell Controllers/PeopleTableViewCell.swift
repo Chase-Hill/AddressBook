fca21282444fe372d8ff9b1bb97ef2636b64535d
Chase <chaseaohill@gmail.com>
2023-02-14 17:45:57 -0500

    updated PeopleTableViewCell

--- AddressBook/Controllers/Cell Controllers/PeopleTableViewCell.swift
+++ AddressBook/Controllers/Cell Controllers/PeopleTableViewCell.swift
@@ -9,4 +9,37 @@ import UIKit
 
 class PeopleTableViewCell: UITableViewCell {
     
+    // MARK: - Outlets
+    
+    @IBOutlet weak var personNameLabel: UILabel!
+    
+    @IBOutlet weak var favoriteButton: UIButton!
+    
+    // MARK: - Properties
+    
+    var person: Person? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    // MARK: - Helper
+    
+    func updateViews() {
+        guard let person = person else {return}
+        personNameLabel.text = person.name
+        
+        let favoriteImageName = person.isFavorite ? "star.fill" : "star"
+        let favoriteImage = UIImage(systemName: favoriteImageName)
+        favoriteButton.setImage(favoriteImage, for: .normal)
+    }
+    
+    
+    
+    // MARK: - Action
+    
+    @IBAction func favoriteButtonTapped(_ sender: Any) {
+        
+    }
+    
 }
